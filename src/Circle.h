d71702ffb66204179256ac0a57996b6458c0c5c2
TommyL123 <TLik@mail.sfsu.edu>
2017-04-25 23:09:30 -0700

    Update Circle.h

--- src/Circle.h
+++ src/Circle.h
@@ -2,79 +2,62 @@
 // Created by Tommy Lik on 4/24/17.
 //
 
-#ifndef TOMMYL123_HW6_CIRCLE_H
-#define TOMMYL123_HW6_CIRCLE_H
+//#ifndef TOMMYL123_HW6_CIRCLE_H
+//#define TOMMYL123_HW6_CIRCLE_H
 
 #include "Point2D.h"
 #include "CustomException.h"
 #pragma once
 
 
-template<class T> class Circle:public Point2D<T>
-{
-
-protected:
-
+template<class T>
+class Circle : public Point2D<T> {
+private:
     T radius;
 
-
 public:
+    Circle() : radius(0.0) {}
 
-    Circle();
-    Circle(T x, T y, T radius);
+    Circle(T x, T y, T newRadius) : Point2D<T>(x, y){
+        radius=newRadius;
+        if( radius<=0){
+            throw CustomException(22);
+        }
+    }
+    T getRadius();
 
-    T getRadius() const;
+    void setRadius(T newRadius);
 
-    void setRadius(T inputRadius);
+    virtual float area() const;
 
-    float area() const;
-
-    float circumference() const;
-
+    virtual float circumference() const;
 
 };
 
-
-template<class T> Circle<T>::Circle() {}
-
-template<class T> Circle<T>::Circle(T x, T y, T radius)
-{
-    if(radius < 0 )
-    {
-        throw CustomException(22);
-    }
-    else
-    {
-        this->x = x;
-        this->y = y;
-        this->radius = radius;
-    }
-}
+template<class T>
 
 
-template<class T> T Circle<T>::getRadius() const
-{
+T Circle<T>::getRadius() {
     return radius;
 }
-template<class T> void Circle<T>::setRadius(T inputRadius)
-{
-    if(inputRadius <= 0)
-    {
-        throw CustomException(22);
-    }
-    else
-    {
-        radius= inputRadius;
-    }
+
+template<class T>
+void Circle<T>::setRadius(T newRadius) {
+    radius = newRadius;
 }
 
-template<class T> float Circle<T>::area() const
-{
-    return (3.14 * (radius * radius));
+
+template<class T>
+float Circle<T>::area() const {
+    const float pi = 3.14;
+    return pi * (radius * radius);
 }
-template<class T> float Circle<T>::circumference() const
-{
-    return (2*3.14*radius);
+
+template<class T>
+float Circle<T>::circumference() const {
+    const float pi = 3.14;
+    return pi * 2 * radius;
+
 }
 
 #endif //TOMMYL123_HW6_CIRCLE_H
