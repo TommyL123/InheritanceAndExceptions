32b41d33030b7f4008f4f2ce99580d50ffca0db9
TommyL123 <TLik@mail.sfsu.edu>
2017-04-25 23:10:45 -0700

    Update Rectangle.h

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -1,92 +1,80 @@
 //
-// Created by Tommy Lik on 4/25/17.
+// Created by Tommy Lik on 4/24/17.
 //
 
-#ifndef TOMMYL123_HW6_RECTANGLE_H
-#define TOMMYL123_HW6_RECTANGLE_H
+//#ifndef TOMMYL123_HW6_RECTANGLE_H
+//#define TOMMYL123_HW6_RECTANGLE_H
 
 #include "Point2D.h"
 #include "CustomException.h"
 #pragma once
 
-template<class T> class Rectangle:public Point2D<T>
-{
+template<class T>
+class Rectangle : public Point2D<T> {
+    
 protected:
+    
     T width;
     T height;
-public:
 
-    Rectangle();
-    Rectangle(T x, T y, T width, T height);
+    
+public:
+    
+    Rectangle() : width(0), height(0) {}
+
+    Rectangle(T x, T y, T nwidth, T nheight) : Point2D<T>(x, y) {
+        
+        width = nwidth;
+        
+        height = nheight;
+        if( height <=0 || width  <=0){
+            
+             
+            throw CustomException(33);
+        }
+    }
 
     T getWidth() const;
+
     T getHeight() const;
-    void setWidth(T inputWidth);
-    void setHeight(T inputHeight);
-    float area() const;
-    float circumference() const;
+
+    void setWidth(T newWidth);
+
+    void setHeight(T newHeight);
+
+    virtual float area () const;
+
+    virtual float circumference() const;
+
 };
 
-template<class T> Rectangle<T>::Rectangle(){}
-template<class T> Rectangle<T>::Rectangle(T x, T y, T width, T height)
-{
-    if(width < 0 || height < 0)
-    {
-        throw CustomException(33);
-    }
-    else
-    {
-        this->x = x;
-        this->y = y;
-        this->width = width;
-        this->height = height;
-    }
-}
-
-template<class T> T Rectangle<T>::getWidth() const
-{
+template<class T>
+T Rectangle<T>::getWidth()const {
     return width;
 }
-template<class T> T Rectangle<T>::getHeight() const
-{
+
+template<class T>
+T Rectangle<T>::getHeight() const {
     return height;
 }
 
-template<class T> void Rectangle<T>::setWidth(T inputWidth)
-{
-    if(inputWidth <=0)
-    {
-        throw CustomException(33);
-    }
-    else
-    {
-        width = inputWidth;
-    }
-}
-template<class T> void Rectangle<T>::setHeight(T inputHeight)
-{
-    if(inputHeight <=0)
-    {
-        throw CustomException(33);
-    }
-    else
-    {
-        height = inputHeight;
-    }
+template<class T>
+void Rectangle<T>::setWidth(T newWidth) {
+    width = newWidth;
 }
 
-template<class T> float Rectangle<T>::area() const
-{
-    return (width * height);
+template<class T>
+void Rectangle<T>::setHeight(T newHeight) {
+    height = newHeight;
 }
-
-template<class T> float Rectangle<T>::circumference() const
-{
-    return ((2*height) + (2*width));
+template <class T>
+float Rectangle<T>::area() const{
+    return width * height;
 }
-
-
-
+template <class T>
+float Rectangle<T>::circumference()const {
+    return (width * 2 + height * 2);
+}
 
 
 #endif //TOMMYL123_HW6_RECTANGLE_H
